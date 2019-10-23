0b7ba917efbb8bab5d72a8c72aac9d7797b268fa
Chris Cowan <agentme49@gmail.com>
2019-10-22 17:48:18 -0700

    make example be function component

--- example/Example.js
+++ example/Example.js
@@ -4,61 +4,73 @@
 import React from 'react';
 import FloatAnchor from '../src';
 
-export default class Example extends React.Component<*> {
-  render() {
-    return (
-      <div className="main">
-        <div className="intro">
-          <p>
-            This is a demonstration of the <a href="https://github.com/StreakYC/react-float-anchor">react-float-anchor</a> library.
-          </p>
-          <p>
-            The "Planets" and "Dwarf Planets" elements are each anchors that
-            have floating elements aligned to them. The floating elements will
-            reposition as necessary if the user scrolls or resizes the page.
-          </p>
-          <div>
-            <FloatAnchor
-              options={{position:'right', vAlign:'top', hAlign: 'left'}}
-              anchor={anchorRef =>
-                <div
-                  style={{border:'1px dashed gray', display:'inline-block'}}
-                  ref={anchorRef}
-                >
-                  Planets
-                </div>
-              }
-              float={
-                <div style={{border:'1px solid black', background:'white'}}>
-                  Mercury<br/>
-                  Venus<br/>
-                  Earth<br/>
-                  Mars<br/>
-                  Jupiter<br/>
-                  Saturn<br/>
-                  Uranus<br/>
-                  Neptune<br/>
-                  <FloatAnchor
-                    options={{position:'right', vAlign:'top', hAlign: 'left'}}
-                    anchor={anchorRef =>
-                      <div ref={anchorRef}>
-                        Dwarf Planets ►
-                      </div>
-                    }
-                    float={
-                      <div style={{border:'1px solid black', background:'white'}}>
-                        Ceres<br/>
-                        Pluto<br/>
-                        Eris
-                      </div>
-                    }
-                  />
-                </div>
-              }
-            />
-          </div>
+export default function Example() {
+  return (
+    <div className="main">
+      <div className="intro">
+        <p>
+          This is a demonstration of the{' '}
+          <a href="https://github.com/StreakYC/react-float-anchor">
+            react-float-anchor
+          </a>{' '}
+          library.
+        </p>
+        <p>
+          The "Planets" and "Dwarf Planets" elements are each anchors that have
+          floating elements aligned to them. The floating elements will
+          reposition as necessary if the user scrolls or resizes the page.
+        </p>
+        <div>
+          <FloatAnchor
+            options={{ position: 'right', vAlign: 'top', hAlign: 'left' }}
+            anchor={anchorRef => (
+              <div
+                style={{ border: '1px dashed gray', display: 'inline-block' }}
+                ref={anchorRef}
+              >
+                Planets
+              </div>
+            )}
+            float={
+              <div style={{ border: '1px solid black', background: 'white' }}>
+                Mercury
+                <br />
+                Venus
+                <br />
+                Earth
+                <br />
+                Mars
+                <br />
+                Jupiter
+                <br />
+                Saturn
+                <br />
+                Uranus
+                <br />
+                Neptune
+                <br />
+                <FloatAnchor
+                  options={{ position: 'right', vAlign: 'top', hAlign: 'left' }}
+                  anchor={anchorRef => (
+                    <div ref={anchorRef}>Dwarf Planets ►</div>
+                  )}
+                  float={
+                    <div
+                      style={{ border: '1px solid black', background: 'white' }}
+                    >
+                      Ceres
+                      <br />
+                      Pluto
+                      <br />
+                      Eris
+                    </div>
+                  }
+                />
+              </div>
+            }
+          />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
